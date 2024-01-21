b42021a96090ab0fb091042da7d112e4cd6437fd
beraqruas <157208343+beraqruas@users.noreply.github.com>
2024-01-21 14:45:29 +0000

    Update qal.sh

--- qal.sh
+++ qal.sh
@@ -6,4 +6,4 @@ sudo wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linu
 sudo tar xvzf ngrok.tgz
 sudo chmod +x ngrok
 ./ngrok authtoken 2bGJGVp2BChIW56SepwdlmMGMUP_nxLTjw8CakmEjHDpr8eU
-./ngrok tcp --region=ap 3389
+
